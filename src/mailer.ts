51564d1d004f43f658ef02a4da93e04db1bc2b08
Arthur Fusco <fuscoarthur1@gmail.com>
2022-09-01 15:45:51 -0300

    configuração de dkim adicionada ao transport do e-mail

--- src/mailer.ts
+++ src/mailer.ts
@@ -4,9 +4,16 @@ async function createTransport() {
   const config = {
     host: process.env.MAILER_HOST,
     port: parseInt(process.env.MAILER_PORT || ""),
+
     auth: {
       user: process.env.MAILER_USER,
       pass: process.env.MAILER_PASS,
+    },
+
+    dkim: {
+      domainName: "russelservicos.com.br",
+      keySelector: "nodemailer",
+      privateKey: process.env.DKIM_KEY || "",
     },
   };
   const transport = nodemailer.createTransport(config);
