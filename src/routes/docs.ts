25caaa1c1189ff11d2316e0347c8699f9dd88f13
Arthur Fusco <fuscoarthur1@gmail.com>
2022-09-08 12:51:13 -0300

    feat: rota para envio dos emails quando os documentos são assinados

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { created } from "../controllers/docs.controllers";
+import { created, docSigned } from "../controllers/docs.controllers";
 
 const router = express.Router();
 
 router.post("/created", created);
+router.post("/signed", docSigned);
 
 export default router;
