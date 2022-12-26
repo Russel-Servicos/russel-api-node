5ffe40ab4bcd8833dd8b954664fe6d8877bac444
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-12-26 10:30:39 -0300

    config: .env.example, docker-compose, jest, package.json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    clearMocks: true,
+    coverageProvider: "v8",
+    coverageReporters: ["text", "lcov"],
+    coverageDirectory: "coverage",
+    collectCoverageFrom: ["src/**/*"],
+    coveragePathIgnorePatterns: ["node_modules", "src/"],
+    testPathIgnorePatterns: ["node_modules", "dist"],
+    transformIgnorePatterns: ["node_modules"],
+    watchPathIgnorePatterns: ["node_modules"],
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    },
 };
