a8c822ab0930da6575a67d0b4b58408d5ad7fa4d
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2023-01-09 10:18:04 -0300

    feat: usa middlewares de logs no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from "express";
 import errorHandler from "./middlewares/errorHandler";
+import requestLogMiddleware from "./middlewares/requestLogMiddleware";
+import responseLogMiddleware from "./middlewares/responseLogMiddleware";
 import router from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(requestLogMiddleware);
 app.use("/api", router);
+app.use(responseLogMiddleware);
 app.use(errorHandler);
 
-app.get('/',(req: Request, res: Response)=>{
-    return res.status(200).json({message:"testando..."});
+app.get("/", (req: Request, res: Response) => {
+    return res.status(200).json({ message: "testando..." });
 });
-
 
 export default app;
